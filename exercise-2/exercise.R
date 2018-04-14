946b91321228cff4527bee77a33540729c1a252f
Gg <hien15@uw.edu>
2018-04-13 21:10:24 -0700

    Complete

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,28 +3,29 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
-
+num <- 1:100
+employee <- paste("employee", num)
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
+salary_2017 <- runif(100, 40000, 50000)
 
 # Create a vector of 100 salaries in 2018 that have changed from 2017
 # Use `runif()` to add a random number between -5000 and 10000 to each of 2017's
 # salaries (the negative number means that a salary may have decreased!)
-
+salary_2018 <- salary_2017 + runif(100, -5000, 10000)
 
 # Create a data frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salary <- data.frame(employee, salary_2017, salary_2018, stringsAsFactors = FALSE)
 
 # Create a column 'change' that stores each person's change in salary between
 # 2017 and 2018
-
+salary$change <- salary_2018 - salary_2017
 
 # Create a column 'got_raise' that is TRUE if the person got a raise (their
 # salary went up)
-
+salary$got_raise <- change > 0
 
 
 ### Retrieve values from your data frame to answer the following questions
@@ -32,7 +33,7 @@
 ### cell rather than the whole row!)
 
 # What was the 2018 salary of employee 57
-
+x <- employee[]
 
 # How many employees got a raise?
 
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,43 +2,49 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data(USPersonalExpenditure)
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+data <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(data)
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(data)
 
 # Add a column "category" to your data frame that contains the rownames
-
+data$category <- rownames(data)
 
 # How much money was spent on personal care in 1940?
-
+data["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
+data["Food and Tobacco", "X1960"]
 
 
 # What was the highest expenditure category in 1960?
-
-
+max <- max(data$X1960)
+data$category[data$X1960 == max]
 # Define a function `lowest_category` that takes in a year as a parameter, and
 # returns the lowest spending category of that year
-
+lowest_category <- function(x) {
+  x <- paste("X", x)
+  min <- min(data$x)
+  data$category[data$x == min]
+}
 
 # Using your function, determine the lowest spending category of each year
 # Hint: use the `sapply()` function to apply your function to a vector of years
-
+vector <- c(1940, 1945, 1950, 1955, 1960)
+sapply(vector, lowest_category)
